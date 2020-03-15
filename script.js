694c61df27ba5133ca619ecd427bba6461d71bbe
Nastya07s <nastya.kotovich07@gmail.com>
2020-03-15 17:12:17 +0300

    feat: add switch screen

--- script.js
+++ script.js
@@ -15,20 +15,20 @@ let current = 0;
 const numberOfSlides = 1;
 slides.forEach(el => el === slides[current] ? el.style.display = 'flex' : el.style.display = 'none');
 
-const changeSlide = () =>{
+const changeSlide = () => {
   current--;
   if (current < 0) {
     current = numberOfSlides;
     slides[current].style.display = 'flex';
     slides[current - 1].style.display = 'none';
     slider.style.backgroundColor = '#648BF0';
-    slider.style.borderBottom =  '6px solid rgb(58, 78, 201)';
-  } else{
+    slider.style.borderBottom = '6px solid rgb(58, 78, 201)';
+  } else {
     slides[current].style.display = 'flex';
     slides[current + 1].style.display = 'none';
     slider.style.backgroundColor = '#f06c64';
-    slider.style.borderBottom =  '6px solid #ea676b';
-  }  
+    slider.style.borderBottom = '6px solid #ea676b';
+  }
 }
 
 const arrowLeft = document.querySelector('.slider  .arrow-right');
@@ -36,3 +36,11 @@ arrowLeft.addEventListener('click', () => changeSlide());
 
 const arrowRight = document.querySelector('.slider  .arrow-left');
 arrowRight.addEventListener('click', () => changeSlide());
+
+const switchScreen = (el) => el.style.zIndex == 2 ? el.style.zIndex = '-1' : el.style.zIndex = '2';
+
+const verticalButton = document.querySelector('.vertical-phone__button');
+verticalButton.addEventListener('click', () => switchScreen(document.querySelector('.vertical-phone__off')))
+
+const horizontalButton = document.querySelector('.horizontal-phone__button');
+horizontalButton.addEventListener('click', () => switchScreen(document.querySelector('.horizontal-phone__off')))
